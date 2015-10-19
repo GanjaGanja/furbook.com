1fa7d70851281fae75c553e17812a88bc77739c8
GanjaGanja <ochenj.gorkyj@users.noreply.github.com>
2015-10-19 15:41:26 +0600

    add basic about page

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -24,3 +24,8 @@ Route::get('cats/{id}', function ($id)
 {
 	return sprintf('Cat #%s', $id);
 })->where('id', '[0-9]+');
+
+Route::get('about', function ()
+{
+	return view('about')->with('number_of_cats', 9000);
+});
